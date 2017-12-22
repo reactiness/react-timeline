becc1a889cb01bee4f285ee5cee807e4cd4ed459
Gavin Gregory <gavin.ian.gregory@gmail.com>
2017-12-22 16:29:22 -0500

    added .edigorconfig file. changed from 4 space to 2 space.

--- webpack.config.js
+++ webpack.config.js
@@ -5,73 +5,72 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin'); // css plugin
 
 // Constant with our paths
 const paths = {
-    DIST: path.resolve(__dirname, 'dist'),
-    SRC: path.resolve(__dirname, 'src'),
-    JS: path.resolve(__dirname, 'src/js'),
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+  JS: path.resolve(__dirname, 'src/js'),
 };
 
 // Webpack configuration
 module.exports = {
-    entry: path.join(paths.JS, 'app.js'),
-    output: {
-        path: paths.DIST,
-        filename: 'app.bundle.js'
-    },
+  entry: path.join(paths.JS, 'app.js'),
+  output: {
+    path: paths.DIST,
+    filename: 'app.bundle.js'
+  },
 
-    // Tell webpack to use html plugin
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(paths.SRC, 'index.html'),
-        }),
-        new ExtractTextPlugin('style.bundle.css'), // CSS will be extracted to this bundle file
+  // Tell webpack to use html plugin
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+    }),
+    new ExtractTextPlugin('style.bundle.css'), // CSS will be extracted to this bundle file
+  ],
+
+  module: {
+    loaders: [
+
+      // Tell webpack to use "babel-loader" for .js and .jsx files. eslint for linting.
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'react-hot-loader!babel-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader', 'eslint-loader']
+      }
     ],
-
-    module: {
-        loaders: [
-            
-            // Tell webpack to use "babel-loader" for .js and .jsx files. eslint for linting.
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'react-hot-loader!babel-loader'
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loaders: ['babel-loader', 'eslint-loader']
-            }
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      // Files will get handled by css loader and then passed to the extract text plugin which will write it to the file defined above in plugins
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: 'css-loader',
+        }),
+      },
+      // File loader for image assets
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          'file-loader',
         ],
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            },
-            // Files will get handled by css loader and then passed to the extract text plugin which will write it to the file defined above in plugins
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                    use: 'css-loader',
-                }),
-            },
-            // File loader for image assets
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    'file-loader',
-                ],
-            },
+      },
+    ],
+  },
 
-        ],
-    },
-
-    //Enable importing JS files without specifying their's extenstion
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
+  //Enable importing JS files without specifying their's extenstion
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
