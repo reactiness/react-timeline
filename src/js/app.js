cce60057d6cd6fb80c435878c65980dff885e6b9
Geoff Whitehead <geoff1012@gmail.com>
2017-12-22 19:51:50 +0000

    wip adding linting with eslint

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,7 @@ export default class Hello extends Component {
   render() {
     return (
       <div>
-        Hello from react
+        Hello from reactaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
         <img src={ keenImage } alt='Commander Keen' />
       </div>
     );
