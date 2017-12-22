82a870cedae44c281c3f586c93541b06cb83c9cc
Geoff Whitehead <geoff1012@gmail.com>
2017-12-22 18:49:31 +0000

    added setup for handling css files

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import '../css/style.css';
+
 export default class Hello extends Component {
   render() {
     return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,53 @@
 const path = require('path');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // html plugin
+const ExtractTextPlugin = require('extract-text-webpack-plugin'); // css plugin
 
 // Constant with our paths
 const paths = {
-  DIST: path.resolve(__dirname, 'dist'),
-  SRC: path.resolve(__dirname, 'src'),
-  JS: path.resolve(__dirname, 'src/js'),
+    DIST: path.resolve(__dirname, 'dist'),
+    SRC: path.resolve(__dirname, 'src'),
+    JS: path.resolve(__dirname, 'src/js'),
 };
 
 // Webpack configuration
 module.exports = {
-  entry: path.join(paths.JS, 'app.js'),
-  output: {
-    path: paths.DIST,
-    filename: 'app.bundle.js'
-  },
+    entry: path.join(paths.JS, 'app.js'),
+    output: {
+        path: paths.DIST,
+        filename: 'app.bundle.js'
+    },
 
-  // Tell webpack to use html plugin
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(paths.SRC, 'index.html'),
-    }),
-  ],
-
-  // We are telling webpack to use "babel-loader" for .js and .jsx files
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
-      },
+    // Tell webpack to use html plugin
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(paths.SRC, 'index.html'),
+        }),
+        new ExtractTextPlugin('style.bundle.css'), // CSS will be extracted to this bundle file
     ],
-  },
 
-  //Enable importing JS files without specifying their's extenstion
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+    module: {
+        rules: [
+            // Tell webpack to use "babel-loader" for .js and .jsx files
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                ],
+            },
+            // Files will get handled by css loader and then passed to the extract text plugin which will write it to the file defined above in plugins
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    use: 'css-loader',
+                }),
+            }
+        ],
+    },
+
+    //Enable importing JS files without specifying their's extenstion
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
 };
