b6f7576846622d6a2708b2cd682894a7e2c0e607
Geoff Whitehead <geoff1012@gmail.com>
2017-12-22 18:44:04 +0000

    bare bones react webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Constant with our paths
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+  JS: path.resolve(__dirname, 'src/js'),
+};
+
+// Webpack configuration
+module.exports = {
+  entry: path.join(paths.JS, 'app.js'),
+  output: {
+    path: paths.DIST,
+    filename: 'app.bundle.js'
+  },
+
+  // Tell webpack to use html plugin
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+    }),
+  ],
+
+  // We are telling webpack to use "babel-loader" for .js and .jsx files
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+
+  //Enable importing JS files without specifying their's extenstion
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
